98071dee0a127033d0c7f33c8b6363fd9d4ff4bb
Pratik Bhadane <pratikbhadane24@gmail.com>
2021-08-16 15:05:51 +0530

    stored highscore

--- Turtle Projects/The Snake Game/scoreboard.py
+++ Turtle Projects/The Snake Game/scoreboard.py
@@ -8,7 +8,8 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0     
-        self.high_score = 0     
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")   
         self.penup()
         self.hideturtle()
@@ -22,6 +23,8 @@ class Score(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0     
         self.updatescoreboard()
     # def gameover(self):
