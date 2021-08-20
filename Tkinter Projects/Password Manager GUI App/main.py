266d5a9613fc639eac252353f42fa3db1f7873aa
Pratik Bhadane <pratikbhadane24@gmail.com>
2021-08-20 15:53:03 +0530

    added randompass and pyerclip(copy to clipboard)

--- Tkinter Projects/Password Manager GUI App/main.py
+++ Tkinter Projects/Password Manager GUI App/main.py
@@ -1,17 +1,39 @@
 from tkinter import *
 from tkinter import messagebox
+from random import randint, choice, shuffle
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
 
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+               'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_syms = [choice(symbols) for _ in range(randint(2, 4))]
+    password_nums = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_syms + password_nums
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
 
 def savedata():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
 
-    if len(website)==0 or len(email)==0 or len(password)==0:
-        messagebox.showinfo(title="Oops!", message="Please make sure you haven't left any field empty!")
+    if len(website) == 0 or len(email) == 0 or len(password) == 0:
+        messagebox.showinfo(
+            title="Oops!", message="Please make sure you haven't left any field empty!")
     else:
         is_ok = messagebox.askokcancel(
             title=website, message=f"Please Confirm Your details\nEmail: {email}\nPassword: {password}\nPress OK to Save.")
@@ -64,7 +86,8 @@ password_entry.grid(row=3, column=1, pady=3)
 
 
 # Buttons
-genpass = Button(text="Generate Password", font=('calibre', 7, 'normal'))
+genpass = Button(text="Generate Password", font=(
+    'calibre', 7, 'normal'), command=generate_password)
 genpass.grid(row=3, column=2)
 
 add = Button(text="Add", width=39, command=savedata)
